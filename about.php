5fbff8a285c04e3bb765e4efbfb624155baee661
Aaron Frey <aaron.frey@gmail.com>
2017-11-20 17:04:57 -0500

    finish the secondary page

--- about.php
+++ about.php
@@ -11,35 +11,43 @@
 
     <div class="welcome col-sm-6 col-md-5 col-lg-4">
       <div class="welcome-content">
-        <a href="/" class="btn btn-default pull-right">Back to Home</a>
+        <a href="/" class="btn btn-default">Back to Home</a>
       </div>
     </div>
 
     <div id="main-content">
-      <div class="container inner-content">
+      <div class="container-fluid inner-content">
         <div class="row">
-          <div class="col-md-4">
-            <img src="#">
+
+          <div class="col-sm-4">
+            <img src="img/about.png" class="img-responsive">
           </div>
-          <div class="col-md-8">
+
+          <div class="col-sm-8">
 
             <h2>Welcome to Fermi Realty</h2>
 
-            <h3>Fermi Realty’s brokerage professionals specialize in:</h3>
-            <ul>
-              <li>Healthcare</li>
-              <li>Office and Retail</li>
-              <li>Self Storage/Flex/Industrial</li>
-              <li>Tenant and Landlord Representation</li>
-              <li>Land Acquisition and Development</li>
-            </ul>
+            <div class="content-block">
+	            <h3>Fermi Realty’s brokerage professionals specialize in:</h3>
+	            <ul>
+	              <li>Healthcare</li>
+	              <li>Office and Retail</li>
+	              <li>Self Storage/Flex/Industrial</li>
+	              <li>Tenant and Landlord Representation</li>
+	              <li>Land Acquisition and Development</li>
+	            </ul>
+            </div>
 
-            <h3>Leasing Services</h3>
-            <p>Effective leasing of commercial real estate requires thoughtful research, marketing, and planning as well as a dedicated team to properly position the property and attract quality tenants. Effective landlord representation will minimize the time that a property remains vacant and maximize your return on your investment.</p>
+            <div class="content-block">
+	            <h3>Leasing Services</h3>
+	            <p>Effective leasing of commercial real estate requires thoughtful research, marketing, and planning as well as a dedicated team to properly position the property and attract quality tenants. Effective landlord representation will minimize the time that a property remains vacant and maximize your return on your investment.</p>
+	          </div>
 
-            <h3>Landlord and Buyer Representation</h3>
-            <p>We understand that each property is distinct, and that each investor has different objectives. Our access to market information, in-house financial analysis, and overall expertise creates a unique brokerage representation experience.</p>
-            <p>Whether the need is for a tax deferred exchange, an owner/user purchase or a transaction that is strictly for investment purposes, our agents will create a plan based on an analysis of the current market and your specific requests. We will find the right property, negotiate the best terms and be available to help you make sure that all due diligence is done thoroughly and completely to ensure a successful close.</p>
+	          <div class="content-block">
+	            <h3>Landlord and Buyer Representation</h3>
+	            <p>We understand that each property is distinct, and that each investor has different objectives. Our access to market information, in-house financial analysis, and overall expertise creates a unique brokerage representation experience.</p>
+	            <p>Whether the need is for a tax deferred exchange, an owner/user purchase or a transaction that is strictly for investment purposes, our agents will create a plan based on an analysis of the current market and your specific requests. We will find the right property, negotiate the best terms and be available to help you make sure that all due diligence is done thoroughly and completely to ensure a successful close.</p>
+	          </div>
 
           </div>
         </div>
